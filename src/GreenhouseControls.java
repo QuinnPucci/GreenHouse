340c8c1af18d089fea048a047c5e9709f457ffcf
Quinn Pucci <pucciquinn@gmail.com>
2026-03-26 09:52:00 -0600

    fans, timed bell, input from txt files.

--- src/GreenhouseControls.java
+++ src/GreenhouseControls.java
@@ -17,7 +17,10 @@
  */
 
 import java.io.*;
-import java.util.Calendar;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 import tme3.*;
 
 
@@ -125,10 +128,6 @@ public class GreenhouseControls extends Controller {
 
     // BELL modifications
 
-    // notes.
-    // ive set up a remaining rings variable that is decremented every time bell is rung.
-    // NOW I need: another bell event is added... every 2000ms .... IF remainingRings is > 0
-
   public class Bell extends Event {
 
     public int remainingRings;
@@ -139,25 +138,72 @@ public class GreenhouseControls extends Controller {
     }
     public void action() {
         remainingRings--; // everytime action is called on bell subtract the remaining rings
+
+        // here is how we handle the every 2 seconds
+        if (remainingRings > 0) {
+            // if there is any remaining rings left, add a new bell event with a delay time of 2s
+            addEvent(new Bell(2000, remainingRings));
+        }
     }
     public String toString() { return "Bing!"; }
   }
 
   public class Restart extends Event {
+        String inputEvents; // make it an instance variable so I can use it in action
     public Restart(long delayTime, String filename) {
-      super(delayTime);
-      eventsFile = filename; // here is the input file variable
+        super(delayTime);
+        this.inputEvents = filename;
     }
 
     public void action() {
-	addEvent(new ThermostatNight(0));
-	addEvent(new LightOn(2000));
-	addEvent(new WaterOff(8000));
-	addEvent(new ThermostatDay(10000));
-	addEvent(new Bell(9000, 5)); // ADDED IN REMAINING RINGS IN THE HARDCODED DATA FOR TESTING.
-	addEvent(new WaterOn(6000));
-	addEvent(new LightOff(4000));
-	addEvent(new Terminate(12000));
+
+        // this is the scanner for opening the file and using the while loops to process it
+        try {
+            File in = new File(inputEvents);
+            Scanner scan = new Scanner(in);
+
+            // I relied on this alot for the regex stuff https://www.w3schools.com/java/java_regex.asp
+            // define the patter im trying to match
+            Pattern pattern = Pattern.compile("Event=(\\w+),time=(\\d+)(?:,rings=(\\d+))?");
+
+            //loop through each line of the file
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                // use regex pattern to extract the right data from each line and use to create an event
+
+                Matcher match = pattern.matcher(line);
+
+                // use a conditional for each type of event, and add event accordingly
+                // Ref: https://docs.oracle.com/javase/8/docs/api/java/util/regex/Matcher.html
+                if (match.matches()) {
+                    String eventName = match.group(1); // event name
+                    long time = Long.parseLong(match.group(2)); // delay time
+
+                    // if it matches, plug in the variables and create the event
+                    if(eventName.equals("Bell")) {
+                        int rings = Integer.parseInt(match.group(3));
+                        addEvent(new Bell(time, rings));
+                    } else if (eventName.equals("LightOn")) {
+                        addEvent(new LightOn(time));
+                    } else if (eventName.equals("LightOff")) {
+                        addEvent(new LightOff(time));
+                    } else if (eventName.equals("WaterOn")) {
+                        addEvent(new WaterOn(time));
+                    } else if (eventName.equals("WaterOff")) {
+                        addEvent(new WaterOff(time));
+                    } else if (eventName.equals("ThermostatNight")) {
+                        addEvent(new ThermostatNight(time));
+                    } else if (eventName.equals("ThermostatDay")) {
+                        addEvent(new ThermostatDay(time));
+                    } else if (eventName.equals("Terminate")) {
+                        addEvent(new Terminate(time));
+                    }
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String toString() {
